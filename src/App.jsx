f2e9e81f43e5506a94deec3a9dd2651ba5ce3e9c
Genaro Garcia <genaro.dev@outlook.com>
2023-02-01 13:51:44 -0600

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,36 @@ function App() {
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="border border-black">
-        <h1>advice #{value.id}</h1>
-        <p className="text-[28px]">"{value.advice}"</p>
-        <div>
-          <img src={DividerMobile} alt="Divider" className="md:hidden" />
+    <div className="flex min-h-screen w-full items-center justify-center bg-dark-blue">
+      <div className="relative mx-4 max-w-lg rounded-xl bg-dark-grayish-blue px-6 text-center">
+        <h1 className="mt-10 uppercase tracking-widest text-neon-green">
+          advice #{value.id}
+        </h1>
+        <p className="py-6 text-[28px] text-light-cyan">
+          &ldquo;{value.advice}&rdquo;
+        </p>
+        <div className="mb-16 inline-block">
+          <img src={DividerMobile} alt="Divider" className="sm:hidden" />
           <img
             src={DividerDesktop}
             alt="Divider"
-            className="hidden md:inline"
+            className="hidden sm:inline"
           />
         </div>
-        <button onClick={handleFetch}>
-          <img src={Dice} alt="Dice" />
-        </button>
+
+        <div className="group relative">
+          <div className="absolute -bottom-8 left-1/2 -translate-x-1/2 rounded-full bg-neon-green p-8 transition duration-1000 group-hover:blur-lg group-hover:duration-200"></div>
+          <button
+            className="absolute -bottom-8 left-1/2 -translate-x-1/2 rounded-full bg-neon-green p-5"
+            onClick={handleFetch}
+          >
+            <img
+              src={Dice}
+              alt="Dice"
+              className="transition duration-1000 group-hover:rotate-180"
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
